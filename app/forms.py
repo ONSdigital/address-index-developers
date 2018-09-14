76e77b21fef32e7b7e1de5b69a3741f52409e6d3
Paul Joel <paul.joel@ons.gsi.gov.uk>
2018-09-14 16:31:03 +0100

    Create forms.py

---
+++ app/forms.py
@@ -0,0 +1,41 @@
+from flask_wtf import FlaskForm
+
+from wtforms import TextField, RadioField, SubmitField
+
+from wtforms import validators, ValidationError
+
+
+class postcodeForm(FlaskForm):
+    input = TextField('Postcode', [validators.DataRequired("Please enter a postcode.")])
+    classificationfilter = TextField('Classification Filter')
+    historical = RadioField('Include historical data?', [validators.Required()], choices=[('true','Yes'),('false','No')], default='true')
+    submit = SubmitField('Search')
+
+class partialForm(FlaskForm):
+    input = TextField('Postcode', [validators.DataRequired("Please enter a postcode.")])
+    classificationFilter = TextField('Classification Filter')
+    historical = RadioField('Include historical data?', [validators.Required()], choices=[('true','Yes'),('false','No')], default='true')
+    submit = SubmitField('Search')
+
+class addressForm(FlaskForm):
+    input = TextField('Address', [validators.Required("Please enter an address.")])
+    classificationFilter = TextField('Classification Filter')
+    historical = RadioField('Include historical data?', [validators.Required()], choices=[('true','Yes'),('false','No')], default='true')
+    submit = SubmitField('Search')
+
+class uprnForm(FlaskForm):
+    uprn = TextField('UPRN', [validators.DataRequired("Required")])
+    historical = TextField('Historical')
+    verbose = TextField('Verbose')
+    submit = SubmitField('Search')
+
+class commonForm(FlaskForm):
+    input = TextField('Input')
+    postcode = TextField('Postcode')
+    uprn = TextField('UPRN')
+    classificationfilter = TextField('Classification Filter')
+    limit = TextField('Limit')
+    offset = TextField('Offset')
+    historical = TextField('Historical')
+    verbose = TextField('Verbose')
+    submit = SubmitField('Search')

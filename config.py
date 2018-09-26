7d7bce807e5493acd6d07d80e24a21d1e0a4cf7f
steve-thorne-ONS <steven.thorne@ons.gov.uk>
2018-09-26 11:37:22 +0100

    Host

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
-# host = "http://addressindex-api-test.apps.devtest.onsclofo.uk"
-host = "0.0.0.0"
+host = "http://addess-index-developers.apps.devtest.onsclofo.uk"
+#host = "0.0.0.0"
 #port = 5000
 swagger_url = "/static/json/ai-swagger.json"

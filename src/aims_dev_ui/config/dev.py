a399df46b7cc126130001b99dfa24ccaa556dbb5
Richard Smith <richard.m.smith@ons.gov.uk>
2021-09-28 14:53:47 +0100

    change to V3 swagger and live API

--- src/aims_dev_ui/config/dev.py
+++ src/aims_dev_ui/config/dev.py
@@ -4,8 +4,9 @@ import os
 
 SECRET_KEY = 'secretkey'
 
-API_URL = os.getenv('API_URL') or 'http://localhost:9000'
+#API_URL = os.getenv('API_URL') or 'http://localhost:9000'
+API_URL = os.getenv('API_URL') or 'https://initial-test-bulk-1.aims.gcp.onsdigital.uk'
 SWAGGER_URL = os.getenv('SWAGGER_URL')
 SWAGGER_PATH = os.getenv('SWAGGER_PATH')
 if not (SWAGGER_URL or SWAGGER_PATH):
-  SWAGGER_URL = API_URL + '/assets/swagger.json'
+  SWAGGER_URL = API_URL + '/openapi/swagger.json'
